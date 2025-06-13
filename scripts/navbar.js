1907f5028c9e06bf7876224e650010345b211e07
Shanoy-Dey <shanoydey09@gmail.com>
2025-06-14 01:42:27 +0530

    commit

---
+++ scripts/navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Check if user is logged in (for example, userId in localStorage)
+  const userId = localStorage.getItem('userId');
+  const navAccount = document.getElementById('nav-account-link');
+  if (navAccount) {
+    if (userId) {
+      navAccount.innerHTML = '<a id="nav-account-link" href="account.html">Account</a>';
+    } else {
+      navAccount.innerHTML = '<a id="nav-account-link" href="login.html">SIGN UP/LOGIN</a>';
+    }
+  }
+});
